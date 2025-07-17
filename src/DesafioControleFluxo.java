883637e25af409d09093d44a8f1289e04b3d35ea
Júlio Prates <juliopfeijo@gmail.com>
2025-07-17 10:02:48 -0300

    docs: adicionar a especificação do desafio

---
+++ src/DesafioControleFluxo.java
@@ -0,0 +1,19 @@
+public class DesafioControleFluxo {
+
+    public static void main(String[] args) {
+        /*
+        ESPECIFICAÇÃO DO DESAFIO:
+        O sistema deverá receber dois parâmetros via terminal que representarão dois números inteiros,
+        com estes dois números você deverá obter a quantidade de interações (for) e realizar a impressão no console
+        dos números incrementados.
+
+        Exemplos:
+
+        Se você passar os números 12 e 30, logo teremos uma interação (for) com 18 ocorrências para imprimir os números,
+        exemplo: "Imprimindo o número 1", "Imprimindo o número 2" e assim por diante.
+
+        Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar a exceção customizada
+        com a seguinte mensagem: "O segundo parâmetro deve ser maior que o primeiro"
+         */
+    }
+}

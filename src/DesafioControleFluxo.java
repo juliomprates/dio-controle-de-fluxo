5a192be254bcf8d6a84da3119cd6302d28676cd4
Júlio Prates <juliopfeijo@gmail.com>
2025-07-17 14:08:55 -0300

    feat: implementar a interação com o usuário

--- src/DesafioControleFluxo.java
+++ src/DesafioControleFluxo.java
@@ -1,4 +1,10 @@
+import exception.InvalidParameterException;
+
+import java.util.Scanner;
+
 public class DesafioControleFluxo {
+
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         /*
@@ -12,8 +18,15 @@ public class DesafioControleFluxo {
         Se você passar os números 12 e 30, logo teremos uma interação (for) com 18 ocorrências para imprimir os números,
         exemplo: "Imprimindo o número 1", "Imprimindo o número 2" e assim por diante.
 
-        Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar a exceção customizada
+        Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar uma exceção customizada
         com a seguinte mensagem: "O segundo parâmetro deve ser maior que o primeiro"
          */
+
+        int n1;
+        int n2;
+        System.out.print("Informe o primeiro número (deve ser menor que o segundo): ");
+        n1 = Integer.parseInt(sc.next());
+        System.out.print("Informe o segundo número (deve ser maior que o primeiro): ");
+        n2 = Integer.parseInt(sc.next());
     }
 }
